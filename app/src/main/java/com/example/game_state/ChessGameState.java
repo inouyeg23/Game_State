9fb11bf766a925bdd16dbe799a8caa668d403d38
ingler24 <ingler24@gmail.com>
2021-03-08 13:57:48 -0800

    Added methods for each instance variable.

--- app/src/main/java/com/example/game_state/ChessGameState.java
+++ app/src/main/java/com/example/game_state/ChessGameState.java
@@ -1,30 +1,42 @@
 package com.example.game_state;
 
+/**
+ * @authors: Jonah Ingler,
+ *
+ *
+ */
+
 public class ChessGameState {
 
     //8x8 array of pieces
     private String[][] board;
 
-    //whose turn it is, player 1(white) or 2(black)
-    int playerToMove;
+    //whose turn it is
+    private int playerTurn;
 
     //variables to see if either player is "checked"
-    boolean isCheckedWhite;
-    boolean isCheckedBlack;
+    private boolean isCheckedWhite;
+    private boolean isCheckedBlack;
 
     //point tally for each player
-    int pointsWhite;
-    int pointsBlack;
+    private int pointsWhite;
+    private int pointsBlack;
 
     //time remaining for each player
-    int secondsWhite;
-    int secondsBlack;
+    private int secondsWhite;
+    private int secondsBlack;
+
+    //is the game paused
+    private boolean isPaused;
+
+    //is there a checkmate
+    private boolean isCheckedmateWhite;
+    private boolean isCheckedmateBlack;
 
     /**
      * Contructor for class ChessGameState
      */
     public ChessGameState(){
-
         //initialize an empty board
         board = new String[8][8];
         for (int i = 0; i < 8; i++) {
@@ -32,7 +44,116 @@ public class ChessGameState {
                 board[i][j] = "  ";
             }
         }
+        //starts at 0
+        playerTurn = 0;
 
+        //nobody starts checked
+        isCheckedBlack = false;
+        isCheckedWhite = false;
 
+        //nobody starts with checkmate
+        isCheckedmateBlack = false;
+        isCheckedmateWhite = false;
+
+        //point tally for each starts at 0
+        pointsBlack = 0;
+        pointsWhite = 0;
+
+        //time starts at 10 minutes, or 600 seconds
+        secondsBlack = 600;
+        secondsWhite = 600;
+
+        //game starts paused
+        isPaused = true;
+    }//constructor
+
+    public String getPiece(int row, int col){
+        if(board == null|| row < 0 || col < 0) {
+            return "Out of Bounds";
+        }
+        if(row >= board.length || col >= board[row].length){
+            return "Out of Bounds";
+        }
+        return board[row][col];
     }
-}
+
+    public void setPiece(int row, int col, String piece){
+        if(board == null|| row < 0 || col < 0) {
+            return;
+        }
+        if(row >= board.length || col >= board[row].length) {
+            return;
+        }
+        board[row][col] = piece;
+    }
+
+    public int getPlayerTurn() {
+        return playerTurn;
+    }
+    public void setPlayerTurn(int player){
+        playerTurn = player;
+    }
+
+    public int getPointsBlack() {
+        return pointsBlack;
+    }
+    public void setPointsBlack(int pointsBlack) {
+        this.pointsBlack = pointsBlack;
+    }
+
+    public int getPointsWhite() {
+        return pointsWhite;
+    }
+    public void setPointsWhite(int pointsWhite) {
+        this.pointsWhite = pointsWhite;
+    }
+
+    public int getSecondsBlack() {
+        return secondsBlack;
+    }
+    public void setSecondsBlack(int secondsBlack) {
+        this.secondsBlack = secondsBlack;
+    }
+
+    public int getSecondsWhite() {
+        return secondsWhite;
+    }
+    public void setSecondsWhite(int secondsWhite) {
+        this.secondsWhite = secondsWhite;
+    }
+
+    public boolean isCheckedBlack() {
+        return isCheckedBlack;
+    }
+    public void setCheckedBlack(boolean checkedBlack) {
+        isCheckedBlack = checkedBlack;
+    }
+
+    public boolean isCheckedWhite() {
+        return isCheckedWhite;
+    }
+    public void setCheckedWhite(boolean checkedWhite) {
+        isCheckedBlack = checkedWhite;
+    }
+
+    public boolean isCheckedmateBlack() {
+        return isCheckedmateBlack;
+    }
+    public void setCheckedmateBlack(boolean checkedmateBlack) {
+        isCheckedmateBlack = checkedmateBlack;
+    }
+
+    public boolean isCheckedmateWhite() {
+        return isCheckedmateWhite;
+    }
+    public void setCheckedmateWhite(boolean checkedmateWhite) {
+        isCheckedmateWhite = checkedmateWhite;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
+    }
+    public void setPaused(boolean paused) {
+        isPaused = paused;
+    }
+}//class ChessGameState
